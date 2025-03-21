1ad181f1dc2fe00e839777392a224d05963a6155
Junryl Maraviles <jmaraviles@kryteriononline.com>
2025-03-21 13:41:49 +0800

    Added login api call using dev1 URL.

--- lib/core/network/auth_service.dart
+++ lib/core/network/auth_service.dart
@@ -7,10 +7,18 @@ class AuthService {
     String username,
     String password,
   ) async {
+
+
+    final token = 'Basic Y2xpZW50SWQxOmNsaWVudFNlY3JldDE=';
+
     final response = await http.post(
-      Uri.parse('https://your-api.com/auth/login'),
-      body: jsonEncode({"username": username, "password": password}),
-      headers: {"Content-Type": "application/json"},
+      Uri.parse(
+        'https://dev1.webassessor.com/WebAssessorWebServices/oauth/token?grant_type=password&username=$username&password=$password',
+      ),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': token,
+      },
     );
 
     if (response.statusCode == 200) {
