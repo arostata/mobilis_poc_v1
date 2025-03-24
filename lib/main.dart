f4f59728e6d3d06ab94a21ee1baed04604f570d5
Junryl Maraviles <jmaraviles@kryteriononline.com>
2025-03-24 14:29:52 +0800

    Added code to allow testing with local web service

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'core/theme/app_theme.dart';
 import 'views/auth/sign_in_screen.dart';
+ import 'dart:io';
+
+ //allow any certificates globally for local testing
+ class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 
 void main() {
+
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MyApp());
 }
 
