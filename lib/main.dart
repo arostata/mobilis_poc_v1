a2652c28dc8d012f48906bf0d0c06f183b49b913
Junryl Maraviles <jmaraviles@kryteriononline.com>
2025-03-25 13:38:26 +0800

    Added back allowing any certificates for local testing.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,18 @@ import 'package:mobilis_poc_v1/views/screens/my_assessment_screen.dart';
 import 'package:mobilis_poc_v1/views/screens/sign_in_screen.dart';
 import 'core/theme/app_theme.dart';
 
+import 'dart:io';
+
+ //allow any certificates globally for local testing
+ class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(AppTheme.getApp(const SignInScreen())); // Dynamically switch app
 }
